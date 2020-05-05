7de23932c96463b32b278e472407e16c8b6313b7
Eric Garnett <egcodes@gmail.com>
2020-05-05 15:21:08 -0400

    implement initial movie data fetch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hooked in')
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '37980481',
+      s: searchTerm
+    }
+  })
+  console.log(response.data)
+}
+
+const input = document.querySelector('input')
+let timeoutId
+const onInput = event => {
+  if (timeoutId) {
+    clearInterval(timeoutId)
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value)
+  }, 3000)
+}
+input.addEventListener('input', onInput)
